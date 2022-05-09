5ea31122fedc954b6fe10186221d237492aa84be
mjaillot <marie.jaillot@hotmail.com>
2022-05-09 11:48:17 +0200

    Test function + removed union

--- generalized_floyd_warshall.c
+++ generalized_floyd_warshall.c
@@ -1,6 +1,7 @@
 //code largely repurposed from HW1
 //#error Please comment out the next two lines under linux, then comment this error
 //#include "stdafx.h"  //Visual studio expects this line to be the first one, comment out if different compiler
+#include <stdint.h>
 #ifdef _WIN32
 #include <windows.h> // Include if under windows
 #endif
@@ -18,6 +19,7 @@
 #include <time.h>
 #include <inttypes.h>
 #include <immintrin.h>
+#include <assert.h>
 
 #ifdef __x86_64__
 #include "tsc_x86.h"
@@ -29,47 +31,77 @@
 #define CALIBRATE
 #define ZERO_PROBABILITY 10 //1/ZERO_PROBABILITY is the probability of an entry in the bit matrix being zero
 
-typedef union {
-    double d;
-    uint64_t u;
-} double_bin_t;
 
-double_bin_t *C;
-
-void fw_max_min(int n) {
+void fw_max_min(double *C, int n) {
     for (size_t k = 0; k < n; k++) {
         for (size_t i = 0; i < n; i++) {
             for (size_t j = 0; j < n; j++) {
-                C[i*n + j].d = max(C[i*n + j].d, min(C[i*n + k].d, C[k*n + j].d));
+                C[i*n + j] = max(C[i*n + j], min(C[i*n + k], C[k*n + j]));
             }
         }
     }
 }
 
-void fw_min_plus(int n) {
+void fw_min_plus(double *C, int n) {
     for (size_t k = 0; k < n; k++) {
         for (size_t i = 0; i < n; i++) {
             for (size_t j = 0; j < n; j++) {
-                C[i*n + j].d = min(C[i*n + j].d, C[i*n + k].d + C[k*n + j].d);
+                C[i*n + j] = min(C[i*n + j], C[i*n + k] + C[k*n + j]);
             }
         }
     }
 }
 
-void opt_fw_min_plus(double C[], int n) {
+void basic_opt_fw_min_plus(double *C, int n) {
+    double c_ik;
+    int j0, j1, j2, j3, i_n, k_n;
+
+    for (size_t k = 0; k < n; k++) {
+        k_n = k*n;
+        for (size_t i = 0; i < n; i++) {
+            i_n = i*n;
+            c_ik = C[i_n + k];
+
+            for (size_t j = 0; j < n-4; j+=4) {
+                j1 = j+1, j2 = j+2, j3 = j+3;
+                C[i_n + j0] = min(C[i_n + j0], c_ik + C[k_n + j0]);
+                C[i_n + j1] = min(C[i_n + j1], c_ik + C[k_n + j1]);
+                C[i_n + j2] = min(C[i_n + j2], c_ik + C[k_n + j2]);
+                C[i_n + j3] = min(C[i_n + j3], c_ik + C[k_n + j3]);
+            }
+            switch(n % 4) {
+                case 1:
+                    C[i_n + (n-1)] = min(C[i_n + (n-1)], c_ik + C[k_n + (n-1)]);
+                    break;
+                case 2:
+                    C[i_n + (n-2)] = min(C[i_n + (n-2)], c_ik + C[k_n + (n-2)]);
+                    C[i_n + (n-1)] = min(C[i_n + (n-1)], c_ik + C[k_n + (n-1)]);
+                    break;
+                case 3:
+                    C[i_n + (n-3)] = min(C[i_n + (n-3)], c_ik + C[k_n + (n-3)]);
+                    C[i_n + (n-2)] = min(C[i_n + (n-2)], c_ik + C[k_n + (n-2)]);
+                    C[i_n + (n-1)] = min(C[i_n + (n-1)], c_ik + C[k_n + (n-1)]);
+                    break;
+            }
+        }
+    }
+}
+
+void vect_fw_min_plus(double *C, int n) {
     int i_n = 0;
 
-    double *c_addr = &C[0], *addr_ij, *addr_ik, *addr_kj;
+    double *addr_ij, *addr_ik, *addr_kj;
     __m256d c_ij, c_ik, c_kj, c2, cmp_lt, res;
     for (size_t k = 0; k < n; k++) {
         for (size_t i = 0; i < n; i++) {
+            addr_ik = &(C[i_n + k]);
+            c_ik = _mm256_load_pd(addr_ik);
+
             for (size_t j = 0; j < n; j+=4) {
-                addr_ij = c_addr + sizeof(double) * (i_n + j);
-                addr_ik = c_addr + sizeof(double) * (i_n + k);
-                addr_kj = c_addr + sizeof(double) * (k*n + j);
+                addr_ij = &(C[i_n + j]);
+                addr_kj = &(C[k*n + j]);
                 
                 c_ij = _mm256_load_pd(addr_ij);
-                c_ik = _mm256_load_pd(addr_ik);
                 c_kj = _mm256_load_pd(addr_kj);
 
                 c2 = _mm256_add_pd(c_ik, c_kj);
@@ -84,17 +116,17 @@ void opt_fw_min_plus(double C[], int n) {
 }
 
 //extremely inefficient to cast at every iteration, find fix with minimal code duplication!
-void fw_or_and(int n) {
+void fw_or_and(double *C, int n) {
     for (size_t k = 0; k < n; k++) {
         for (size_t i = 0; i < n; i++) {
             for (size_t j = 0; j < n; j++) {
-                C[i*n + j].u = C[i*n + j].u | (C[i*n + k].u & C[k*n + j].u);
+                C[i*n + j] = (uint64_t)C[i*n + j] | ((uint64_t)C[i*n + k] & (uint64_t)C[k*n + j]);
             }
         }
     }
 }
 
-void opt_fw_or_and_256(double C[], int n) {
+void opt_fw_or_and_256(double *C, int n) {
     int i_n = 0;
 
     double *c_addr = &C[0], *addr_ij, *addr_ik, *addr_kj;
@@ -126,50 +158,61 @@ void opt_fw_or_and_256(double C[], int n) {
     }
 }
 
-void opt_fw_or_and_512(double C[], int n) {
-    int i_n = 0;
+// void opt_fw_or_and_512(double *C, int n) {
+//     int i_n = 0;
 
-    double *c_addr = &C[0], *addr_ij, *addr_ik, *addr_kj;
-    __m512d c_ij, c_ik, c_kj, c2, cmp_lt, res;
-    for (size_t k = 0; k < n; k++) {
-        for (size_t i = 0; i < n; i++) {
-            size_t j = 0;
-            for (; j < n; j+=8) {
-                addr_ij = c_addr + sizeof(double) * (i_n + j);
-                addr_ik = c_addr + sizeof(double) * (i_n + k);
-                addr_kj = c_addr + sizeof(double) * (k*n + j);
+//     double *c_addr = &C[0], *addr_ij, *addr_ik, *addr_kj;
+//     __m512d c_ij, c_ik, c_kj, c2, cmp_lt, res;
+//     for (size_t k = 0; k < n; k++) {
+//         for (size_t i = 0; i < n; i++) {
+//             size_t j = 0;
+//             for (; j < n; j+=8) {
+//                 addr_ij = c_addr + sizeof(double) * (i_n + j);
+//                 addr_ik = c_addr + sizeof(double) * (i_n + k);
+//                 addr_kj = c_addr + sizeof(double) * (k*n + j);
                 
-                c_ij = _mm512_load_pd(addr_ij);
-                c_ik = _mm512_load_pd(addr_ik);
-                c_kj = _mm512_load_pd(addr_kj);
+//                 c_ij = _mm512_load_pd(addr_ij);
+//                 c_ik = _mm512_load_pd(addr_ik);
+//                 c_kj = _mm512_load_pd(addr_kj);
 
-                c2 = _mm512_and_pd(c_ik, c_kj);
-                res = _mm512_or_pd(c_ij, c2);
-                _mm512_store_pd(addr_ij, res);
-            }
-            for(;j < n; j++){
-                u_int64_t c_ij_bits = *(u_int64_t *)(&C[i_n + j]);
-                u_int64_t c_ik_bits = *(u_int64_t *)(&C[i_n + k]);
-                u_int64_t c_kj_bits = *(u_int64_t *)(&C[k*n + j]);
-                C[i_n + j] = c_ij_bits | (c_ik_bits & c_kj_bits); //maybe is wrong cause it gets converted to double
-            }
-            i_n += n;
-        }
-    }
-}
+//                 c2 = _mm512_and_pd(c_ik, c_kj);
+//                 res = _mm512_or_pd(c_ij, c2);
+//                 _mm512_store_pd(addr_ij, res);
+//             }
+//             for(;j < n; j++){
+//                 u_int64_t c_ij_bits = *(u_int64_t *)(&C[i_n + j]);
+//                 u_int64_t c_ik_bits = *(u_int64_t *)(&C[i_n + k]);
+//                 u_int64_t c_kj_bits = *(u_int64_t *)(&C[k*n + j]);
+//                 C[i_n + j] = c_ij_bits | (c_ik_bits & c_kj_bits); //maybe is wrong cause it gets converted to double
+//             }
+//             i_n += n;
+//         }
+//     }
+// }
 
-void init_matrix(int n) {
+void init_matrix(double *C, int n) {
     for (size_t i = 0; i < n; i++) {
         for (size_t j = 0; j < n; j++) {
-            C[i * n + j].d = ((double )rand() + 1)/RAND_MAX;
+            C[i * n + j] = ((double )rand() + 1)/RAND_MAX;
         }
     }   
 }
 
-void init_bit_matrix(int n) {
+void init_matrices(double *C1, double *C2, int n) {
+    double x;
     for (size_t i = 0; i < n; i++) {
         for (size_t j = 0; j < n; j++) {
-            C[i * n + j].d = (rand() % ZERO_PROBABILITY);
+            x = ((double )rand() + 1)/RAND_MAX;
+            C1[i * n + j] = x;
+            C2[i * n + j] = x;
+        }
+    }   
+}
+
+void init_bit_matrix(double *C, int n) {
+    for (size_t i = 0; i < n; i++) {
+        for (size_t j = 0; j < n; j++) {
+            C[i * n + j] = (rand() % ZERO_PROBABILITY);
         }
     }   
 }
@@ -178,7 +221,7 @@ void init_bit_matrix(int n) {
  * Timing function based on the TimeStep Counter of the CPU.
  */
 #ifdef __x86_64__
-double rdtsc(int n, void (*compute)(int)) {
+double rdtsc(double *C, int n, void (*compute)(double*, int)) {
     int i, num_runs;
     myInt64 cycles;
     myInt64 start;
@@ -194,7 +237,7 @@ double rdtsc(int n, void (*compute)(int)) {
     while(num_runs < (1 << 14)) {
         start = start_tsc();
         for (i = 0; i < num_runs; ++i) {
-            compute(n);
+            compute(C, n);
         }
         cycles = stop_tsc(start);
 
@@ -206,7 +249,7 @@ double rdtsc(int n, void (*compute)(int)) {
 
     start = start_tsc();
     for (i = 0; i < num_runs; ++i) {
-        compute(n);
+        compute(C, n);
     }
 
     cycles = stop_tsc(start)/num_runs;
@@ -216,13 +259,38 @@ double rdtsc(int n, void (*compute)(int)) {
 
 //If you want to use a different timer, simply call this function with a different timer function
 //Written like this to enable future extensions
-double benchmark(int n, void (*init_matrix) (int), double (*timer) (int, void (int)), void (*compute)(int)) {
-    init_matrix(n);
-    return timer(n, compute);
+double benchmark(double* C, int n, void (*init_matrix) (double*, int), double (*timer) (double*, int, void (double*, int)), void (*compute)(double*, int)) {
+    init_matrix(C, n);
+    return timer(C, n, compute);
 }
 
-static void (*fw[3]) (int) = {fw_min_plus, fw_or_and, fw_max_min};
-static void (*init[3])(int) = {init_matrix, init_bit_matrix, init_matrix};
+void test(int n, void (*baseline)(double*, int), void (*optimization)(double*, int)) {
+    double *C_base = (double *)malloc(n*n*sizeof(double));
+    double *C_opt = (double *)malloc(n*n*sizeof(double));
+    init_matrices(C_base, C_opt, n);
+    // Run baseline function on C
+    baseline(C_base, n);
+    // Run optimized function on C
+    optimization(C_opt, n);
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            printf("base[%d][%d] = %lf ", i, j, C_base[n*i + j]);
+            printf("opt[%d][%d] = %lf\n", i, j, C_opt[n*i + j]);
+        }
+    }
+    
+    // Compare both
+    for(int i = 0; i < n; ++i) {
+        assert(C_opt[i] == C_base[i]);
+    }
+
+    free(C_base);
+    free(C_opt);
+}
+
+static void (*fw[3]) (double*, int) = {fw_min_plus, fw_or_and, fw_max_min};
+static void (*init[3])(double*, int) = {init_matrix, init_bit_matrix, init_matrix};
 static char msg[3][10] = {"(min, +)", "(and, or)", "(max, min)"};
 
 int main(int argc, char **argv) {
@@ -230,17 +298,21 @@ int main(int argc, char **argv) {
     int n = atoi(argv[1]);
     int fwi = atoi(argv[2]);
     printf("n=%d \n",n);
-    C = (double_bin_t *)malloc(n*n*sizeof(double));
+    double *C = (double *)malloc(n*n*sizeof(double));
 
 #ifdef __x86_64__
-    double r = benchmark(n, init[fwi], rdtsc, fw[fwi]);
+    double r = benchmark(C, n, init[fwi], rdtsc, fw[fwi]);
     printf("%s\n", msg[fwi]);
     printf(" FW : RDTSC instruction:\n %lf cycles measured\n\n", r);
 #endif
 
     for(int i = 0; i < n; i++)
         for(int j = 0; j < n; j++)
-            printf("%lf", C[n*i + j].d);
+            printf("%lf", C[n*i + j]);
+
+    // test(n, fw_min_plus, basic_opt_fw_min_plus);
+
+    free(C);
 
     return 0;
 }
