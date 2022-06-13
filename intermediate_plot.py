2d7342ce622ffbe71ce06d8ffe2222840120b885
mjaillot <marie.jaillot@hotmail.com>
2022-06-13 13:38:03 +0200

    Last results

--- intermediate_plot.py
+++ intermediate_plot.py
@@ -1,196 +1,198 @@
 import matplotlib.pyplot as plt
 
 x = [4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
-x_1024 = [4, 8, 16, 32, 64, 128, 256, 512, 1024]
-x_O0 = [4, 8, 16, 32, 64, 128, 256]
 
-y_min_plus_baseline = [0.606635,
-                       0.993403,
-                       1.378475,
-                       1.568875,
-                       1.690234,
-                       1.644125,
-                       1.693905,
-                       1.230458,
-                       0.753746,
-                       0.693780,
-                       0.709773]
+y_min_plus_baseline = [0.633663,
+                       0.841413,
+                       1.002325,
+                       1.09367,
+                       1.175905,
+                       1.131389,
+                       1.14603,
+                       1.147604,
+                       1.045012,
+                       0.85719,
+                       0.816123
+                       ]
 
-y_min_plus_baseline_O0 = [0.17302,
-                          0.227283,
-                          0.216871,
-                          0.202217,
-                          0.22129,
-                          0.179687,
-                          0.143159
-                          ]
+y_min_plus_opt = [0.684492,
+                  0.773414,
+                  0.777156,
+                  0.827611,
+                  0.837963,
+                  0.803499,
+                  0.819047,
+                  0.835346,
+                  0.75816,
+                  0.660645,
+                  0.658655
+                  ]
 
-y_min_plus_opt = [0.807062,
-                 0.968963,
-                 1.158469,
-                 1.209638,
-                 1.237534,
-                 0.990288,
-                 0.720366,
-                 0.609435,
-                 0.598687,
-                 0.535071,
-                 0.529993]
+y_min_plus_tiled = [0.043956,
+                    0.185675,
+                    0.083806,
+                    2.230557,
+                    0.278412,
+                    0.292648,
+                    1.316907,
+                    0.073953,
+                    0.721084,
+                    1.187293,
+                    0.268666
+                    ]
 
-y_min_plus_opt_O0 = [0.156174,
-                     0.202252,
-                     0.163699,
-                     0.118519,
-                     0.115895,
-                     0.114807,
-                     0.116]
-
-# y_min_plus_tiled = [0.061104,
-#                    0.289789,
-#                    0.118275,
-#                    2.949813,
-#                    0.395441,
-#                    0.333805,
-#                    1.113129,
-#                    0.057261,
-#                    0.53503]
-
-y_min_plus_tiled = [0.064013,
-                    0.291406,
-                    0.116099,
-                    2.222825,
-                    0.231871,
-                    0.2278,
-                    0.996664,
-                    0.063114,
-                    0.597108]
-
-y_min_plus_tiled_vect= [0.011541,
-                        0.069353,
-                        0.042526,
-                        1.935419,
-                        0.157113,
-                        0.142539,
-                        1.005321,
-                        0.030018,
-                        1.444535]
-
-plt.plot(x, y_min_plus_baseline, label="baseline", linewidth=2.0)
-plt.plot(x, y_min_plus_opt, label="basic optimization", linewidth=2.0)
-plt.plot(x_1024, y_min_plus_tiled, label="tiled", linewidth=2.0)
-plt.plot(x_1024, y_min_plus_tiled_vect, label="tiled vectorized", linewidth=2.0)
-# plt.plot(x_O0, y_min_plus_baseline_O0, label="baseline, flag -O0", linewidth=2.0)
-# plt.plot(x_O0, y_min_plus_opt_O0, label="basic optimization, flag -O0", linewidth=2.0)
-plt.legend()
-plt.title("min plus base optimization")
-plt.xscale('log')
-plt.xlabel("N")
-plt.ylabel("flops/cycle")
-plt.savefig("./plots/min_plus_base_opt")
-
-#==============================================================
-y_max_min_baseline = [0.644512,
-                      1.192917,
-                      1.340972,
-                      1.616624,
-                      1.735605,
-                      1.653051,
-                      1.697671,
-                      1.754933,
-                      1.338690,
-                      0.792861,
-                      0.672087]
-
-y_max_min_baseline_O0 = [0.090395,
-                         0.091512,
-                         0.086091,
-                         0.083615,
-                         0.079356,
-                         0.081099,
-                         0.080396]
-
-y_max_min_opt = [0.523303,
-                 0.573927,
-                 0.619274,
-                 0.690119,
-                 0.721193,
-                 0.671105,
-                 0.693432,
-                 0.690820,
-                 0.631541,
-                 0.540784,
-                 0.534826]
-
-y_max_min_opt_O0 = [0.1,
-                    0.1067,
-                    0.108453,
-                    0.104666,
-                    0.097379,
-                    0.102803,
-                    0.101958]
-
-
-# # plt.plot(x, y_max_min_baseline, label="baseline", linewidth=2.0)
-# # plt.plot(x, y_max_min_opt, label="basic optimization", linewidth=2.0)
-# plt.plot(x_O0, y_max_min_baseline_O0, label="baseline, flag -O0", linewidth=2.0)
-# plt.plot(x_O0, y_max_min_opt_O0, label="basic opt, flag -O0", linewidth=2.0)
-# plt.legend()
-# plt.title("max min base optimization")
-# plt.xscale('log')
-# plt.xlabel("N")
-# plt.ylabel("flops/cycle")
-# plt.savefig("./plots/max_min_base_opt")
-
-#==============================================================
-
-y_or_and_baseline = [0.416667,
-                     0.578858,
-                     0.738271,
-                     0.913595,
-                     1.012184,
-                     0.954492,
-                     0.988117,
-                     0.990364,
-                     0.888463,
-                     0.728210,
-                     0.694072]
-
-y_or_and_baseline_O0 = [0.155794,
-                        0.15987,
-                        0.166228,
-                        0.171097,
-                        0.17807,
-                        0.169556,
-                        0.165158
+y_min_plus_tiled_vect= [0.035526,
+                        0.128369,
+                        0.064637,
+                        2.76325,
+                        0.210248,
+                        0.197561,
+                        1.4197,
+                        0.04444,
+                        2.132966,
+                        1.306201,
+                        0.157309
                         ]
 
-y_or_and_opt = [0.50513,
-                0.548356,
-                0.606734,
-                0.708268,
-                0.768382,
-                0.735261,
-                0.720346,
-                0.724475,
-                0.659623,
-                0.552817,
-                0.549917]
+plt.plot(x, y_min_plus_baseline, label="baseline", marker='o')
+plt.plot(x, y_min_plus_opt, label="basic optimization", marker='o')
+plt.plot(x, y_min_plus_tiled, label="tiled", marker='o')
+plt.plot(x, y_min_plus_tiled_vect, label="tiled vectorized", marker='o')
+plt.legend(bbox_to_anchor=(1.38, 1.02))
+plt.suptitle("(min, +) performances", fontweight="bold", fontsize=12, x=0.285)
+plt.title("flags: -march=native -O3 -ffast-math", loc='left')
+plt.xscale('log', base=2)
+plt.xlabel("N")
+plt.ylabel("flops/cycle")
+plt.savefig("./plots/min_plus_performances.pdf", bbox_inches="tight")
 
-y_or_and_opt_O0 = [0.107149,
-                   0.126507,
-                   0.134511,
-                   0.143284,
-                   0.143353,
-                   0.1455,
-                   0.143868]
+#==============================================================
+y_max_min_baseline = [0.61244,
+                      0.855472,
+                      1.006759,
+                      1.143495,
+                      1.237267,
+                      1.138247,
+                      1.146389,
+                      1.21808,
+                      1.162767,
+                      0.879274,
+                      0.83987
+                      ]
 
-# # plt.plot(x, y_or_and_baseline, label="baseline", linewidth=2.0)
-# # plt.plot(x, y_or_and_opt, label="basic optimization", linewidth=2.0)
-# plt.plot(x_O0, y_or_and_baseline_O0, label="baseline, flag -O0", linewidth=2.0)
-# plt.plot(x_O0, y_or_and_opt_O0, label="basic optimization, flag -O0", linewidth=2.0)
-# plt.legend()
-# plt.title("or and base optimization")
-# plt.xscale('log')
+y_max_min_opt = [0.649746,
+                 0.720619,
+                 0.76148,
+                 0.832764,
+                 0.844169,
+                 0.806621,
+                 0.833565,
+                 0.837667,
+                 0.784712,
+                 0.645265,
+                 0.678994
+                 ]
+
+y_max_min_tiled = [0.056313,
+                   0.266251,
+                   0.124268,
+                   1.845201,
+                   0.287868,
+                   0.333735,
+                   1.125903,
+                   0.119227,
+                   0.721661,
+                   1.220366,
+                   0.444199
+                   ]
+
+y_max_min_tiled_vect = [0.036607,
+                        0.136719,
+                        0.063257,
+                        2.659935,
+                        0.212769,
+                        0.190021,
+                        1.40029,
+                        0.040739,
+                        2.022769,
+                        1.163224,
+                        0.128313
+                        ]
+
+# plt.plot(x, y_max_min_baseline, label="baseline", marker='o')
+# plt.plot(x, y_max_min_opt, label="basic optimization", marker='o')
+# plt.plot(x, y_max_min_tiled, label="tiled", marker='o')
+# plt.plot(x, y_max_min_tiled_vect, label="tiled vectorized", marker='o')
+# plt.legend(bbox_to_anchor=(1.38, 1.02))
+# plt.suptitle("(max, min) performances", fontweight="bold", fontsize=12, x=0.31)
+# plt.title("flags: -march=native -O3 -ffast-math", loc='left')
+# plt.xscale('log', base=2)
 # plt.xlabel("N")
 # plt.ylabel("flops/cycle")
-# plt.savefig("./plots/or_and_base_opt")
+# plt.savefig("./plots/max_min_performances.svg", bbox_inches="tight")
+
+#==============================================================
+
+y_or_and_baseline = [0.719101,
+                     0.905393,
+                     1.073235,
+                     1.252552,
+                     1.295312,
+                     1.219713,
+                     1.244866,
+                     1.255767,
+                     1.163521,
+                     0.876437,
+                     0.841047
+                     ]
+
+y_or_and_opt = [0.688172,
+                0.747991,
+                0.752043,
+                0.693011,
+                0.851258,
+                0.800325,
+                0.81615,
+                0.82648,
+                0.776472,
+                0.641221,
+                0.645115
+                ]
+
+y_or_and_tiled = [0.05699,
+                  0.257222,
+                  0.114975,
+                  1.840485,
+                  0.253245,
+                  0.312642,
+                  1.113622,
+                  0.115247,
+                  0.719165,
+                  1.22158,
+                  0.425342
+                  ]
+
+y_or_and_tiled_vect = [0.042314,
+                       0.12776,
+                       0.073844,
+                       1.913907,
+                       0.242062,
+                       0.187179,
+                       1.192032,
+                       0.044487,
+                       1.983928,
+                       1.313406,
+                       0.146329
+                       ]
+
+# plt.plot(x, y_or_and_baseline, label="baseline", marker='o')
+# plt.plot(x, y_or_and_opt, label="basic optimization", marker='o')
+# plt.plot(x, y_or_and_tiled, label="tiled", marker='o')
+# plt.plot(x, y_or_and_tiled_vect, label="tiled vectorized", marker='o')
+# plt.legend(bbox_to_anchor=(1.38, 1.02))
+# plt.suptitle("(or, and) performances", fontweight="bold", fontsize=12, x=0.29)
+# plt.title("flags: -march=native -O3 -ffast-math", loc='left')
+# plt.xscale('log', base=2)
+# plt.xlabel("N")
+# plt.ylabel("flops/cycle")
+# plt.savefig("./plots/or_and_performances.svg", bbox_inches="tight")
