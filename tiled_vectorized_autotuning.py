257d8aba6a514e04b308b469d93699888528bbbb
mjaillot <marie.jaillot@hotmail.com>
2022-06-21 22:51:52 +0200

    Handle different semirings

--- tiled_vectorized_autotuning.py
+++ tiled_vectorized_autotuning.py
@@ -20,21 +20,16 @@ print("Benchmarking " + str(fw[0]))
 
 run("gcc -o ffw " + file_abs_path + " tsc_x86.h -march=native -O3 -ffast-math", shell=True)
 
-x = []
-y = []
 MIN_N = 8
 MAX_N = (2**12)
-n = MIN_N
 MIN_L1 = 4
 MAX_L1 = 400
-L1 = MIN_L1
 MIN_B = 2
 MAX_B = 400
-B = MIN_B
-res = []
-tmp_cycles = 0.0
 
 for fwi in range(3):
+    tmp_cycles = 0.0
+    res = []
     n = MIN_N
     while n <= MAX_N:
         res_for_n = []
@@ -65,7 +60,7 @@ for fwi in range(3):
 
                             # Print benchmark
                             print("Done benchmarking " + str(fw[fwi]) + " with N = %d, L1 = %d, and B = %d" % (n, L1, B))
-                        except:
+                        except IndexError:
                             B = B + 1
                             continue
 
@@ -90,41 +85,41 @@ for fwi in range(3):
         # Update N
         n = n * 3
 
-# Compile all of the best configs and write them to a file
-bestRes = []             # Store best results for each n
-cur_n = 4                # Keep track of current n
-best_flops = -1.0          # Keep track of current best perf
-cur_best_config = (0, 0) # L1 & B
+    # Compile all of the best configs and write them to a file
+    bestRes = []             # Store best results for each n
+    cur_n = 4                # Keep track of current n
+    best_flops = -1.0          # Keep track of current best perf
+    cur_best_config = (0, 0) # L1 & B
 
-for (flops, cycles, _n, _L1, _B) in res:
-    # Find the best config for each n
-    if cur_n == _n:
-        # print("\nBEST: %lf\nFLOPS: %lf\n" % (best_flops, flops))
-        # check for an improved performance
-        if flops >= best_flops:
-            # print("\nOLD_BEST: %lf\nNEW_BEST: %lf\n" % (best_flops, flops))
+    for (flops, cycles, _n, _L1, _B) in res:
+        # Find the best config for each n
+        if cur_n == _n:
+            # print("\nBEST: %lf\nFLOPS: %lf\n" % (best_flops, flops))
+            # check for an improved performance
+            if flops >= best_flops:
+                # print("\nOLD_BEST: %lf\nNEW_BEST: %lf\n" % (best_flops, flops))
+                best_flops = flops
+                cur_best_config = (_L1, _B)
+        else:
+            # Write the previous best_n to the array
+            (best_l1, best_b) = cur_best_config
+            bestRes.append((cur_n, best_l1, best_b, best_flops))
+
+            # Prepare next n
+            cur_n = _n
             best_flops = flops
             cur_best_config = (_L1, _B)
-    else:
-        # Write the previous best_n to the array
-        (best_l1, best_b) = cur_best_config
-        bestRes.append((cur_n, best_l1, best_b, best_flops))
 
-        # Prepare next n 
-        cur_n = _n
-        best_flops = flops
-        cur_best_config = (_L1, _B)
+        # Write all results to a file and to stdout
+        str_res = "RESULT: N = %d, L1 = %d, B = %d:\n%d [cycles], %lf [flops/cycle]\n" % (_n, _L1, _B, cycles, flops)
+        print(str_res)
 
-    # Write all results to a file and to stdout
-    str_res = "RESULT: N = %d, L1 = %d, B = %d:\n%d [cycles], %lf [flops/cycle]\n" % (_n, _L1, _B, cycles, flops)
-    print(str_res)
+    # Write the final best_n to the array
+    best_l1, best_b = cur_best_config
+    bestRes.append((cur_n, best_l1, best_b, best_flops))
 
-# Write the final best_n to the array
-best_l1, best_b = cur_best_config
-bestRes.append((cur_n, best_l1, best_b, best_flops))
-
-# Write all best results to a separate file
-with open("best_results.csv", "w") as best_res_file:
-    for (best_n, best_l1, best_b, final_flops) in bestRes:
-        best_res_file.write("%d, %d, %d, %lf\n" % (best_n, best_l1, best_b, final_flops))
+    # Write all best results to a separate file
+    with open("best_results.csv", "w") as best_res_file:
+        for (best_n, best_l1, best_b, final_flops) in bestRes:
+            best_res_file.write("%s, %d, %d, %d, %lf\n" % (fw[fwi], best_n, best_l1, best_b, final_flops))
 
