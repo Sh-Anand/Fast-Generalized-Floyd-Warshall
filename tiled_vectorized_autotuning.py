6f953e6d99ca5fd53bcc7d680d51977ec1798f33
mjaillot <marie.jaillot@hotmail.com>
2022-06-21 22:24:28 +0200

    Autotune for 3 semirings

--- tiled_vectorized_autotuning.py
+++ tiled_vectorized_autotuning.py
@@ -22,8 +22,9 @@ run("gcc -o ffw " + file_abs_path + " tsc_x86.h -march=native -O3 -ffast-math", 
 
 x = []
 y = []
+MIN_N = 8
 MAX_N = (2**12)
-n = 8
+n = MIN_N
 MIN_L1 = 4
 MAX_L1 = 400
 L1 = MIN_L1
@@ -33,55 +34,57 @@ B = MIN_B
 res = []
 tmp_cycles = 0.0
 
-while n <= MAX_N:
-    res_for_n = []
-    L1 = MIN_L1
-    while L1 <= n and L1 <= MAX_L1:
-        if n % L1 == 0:
-            B = MIN_B
-            while B <= L1 and B <= MAX_B:
-                if L1 % B == 0:
-                    tmp_cycles = 0.0
-                    for i in range(repetitions_for_confidence):
+for fwi in range(3):
+    n = MIN_N
+    while n <= MAX_N:
+        res_for_n = []
+        L1 = MIN_L1
+        while L1 <= n and L1 <= MAX_L1:
+            if n % L1 == 0:
+                B = MIN_B
+                while B <= L1 and B <= MAX_B:
+                    if L1 % B == 0:
+                        tmp_cycles = 0.0
+                        for i in range(repetitions_for_confidence):
 
-                        output = run("%s %d %d %d" % (executable_abs_path, n, L1, B), capture_output=True, shell=True).stdout.decode("utf-8")
+                            output = run("%s %d %d %d %d" % (executable_abs_path, n, fwi, L1, B), capture_output=True, shell=True).stdout.decode("utf-8")
 
-                        cycles = [float(t) for t in output.split() if re.match(r'^-?\d+(?:\.\d+)$', t) is not None][0]
-                        tmp_cycles = tmp_cycles + cycles
+                            cycles = float(output.split()[0])
+                            tmp_cycles = tmp_cycles + cycles
 
-                    # Average result over repetitions
-                    tmp_cycles = tmp_cycles / repetitions_for_confidence
-                    res.append(((2*(n**3) / tmp_cycles), tmp_cycles, n, L1, B))
-                    res_for_n.append(((2*(n**3) / tmp_cycles), tmp_cycles, n, L1, B))
+                        # Average result over repetitions
+                        tmp_cycles = tmp_cycles / repetitions_for_confidence
+                        res.append(((2*(n**3) / tmp_cycles), tmp_cycles, n, L1, B))
+                        res_for_n.append(((2*(n**3) / tmp_cycles), tmp_cycles, n, L1, B))
 
-                    # Store results in case of crash
-                    with open("result_dump.csv", "a") as res_dump_file:
-                        csv_res = "%d, %d, %d, %d, %lf\n" % (n, L1, B, tmp_cycles, (2*(n**3) / tmp_cycles))
-                        res_dump_file.write(csv_res)
+                        # Store results in case of crash
+                        with open("result_dump.csv", "a") as res_dump_file:
+                            csv_res = "%s, %d, %d, %d, %d, %lf\n" % (fw[fwi], n, L1, B, tmp_cycles, (2*(n**3) / tmp_cycles))
+                            res_dump_file.write(csv_res)
 
-                    # Print benchmark
-                    print("Done benchmarking " + str(fw[0]) + " with N = %d, L1 = %d, and B = %d" % (n, L1, B))
+                        # Print benchmark
+                        print("Done benchmarking " + str(fw[fwi]) + " with N = %d, L1 = %d, and B = %d" % (n, L1, B))
 
                     # Update B
                     B = B + 1
-        # Update L1
-        L1 = L1 + 1
+            # Update L1
+            L1 = L1 + 1
 
-    # Compute the best parameters for the past n
-    best_flops_for_n = -1.0
-    best_config_for_n = (0, 0)
-    for (tmp_flops, tmp_cycles, _, tmp_l1, tmp_b) in res_for_n:
-        if tmp_flops >= best_flops_for_n:
-            best_flops_for_n = tmp_flops
-            best_config_for_n = (tmp_l1, tmp_b)
+        # Compute the best parameters for the past n
+        best_flops_for_n = -1.0
+        best_config_for_n = (0, 0)
+        for (tmp_flops, tmp_cycles, _, tmp_l1, tmp_b) in res_for_n:
+            if tmp_flops >= best_flops_for_n:
+                best_flops_for_n = tmp_flops
+                best_config_for_n = (tmp_l1, tmp_b)
 
-    # Write the best results to a file in case of crash
-    with open("best_result_dump.csv", "a") as best_res_dump_file:
-        (best_l1_for_n, best_b_for_n) = best_config_for_n
-        best_res_dump_file.write("%d, %d, %d, %lf\n" % (n, best_l1_for_n, best_b_for_n, best_flops_for_n))
+        # Write the best results to a file in case of crash
+        with open("best_result_dump.csv", "a") as best_res_dump_file:
+            (best_l1_for_n, best_b_for_n) = best_config_for_n
+            best_res_dump_file.write("%d, %d, %d, %lf\n" % (n, best_l1_for_n, best_b_for_n, best_flops_for_n))
 
-    # Update N
-    n = n * 3
+        # Update N
+        n = n * 3
 
 # Compile all of the best configs and write them to a file
 bestRes = []             # Store best results for each n
